a7b91d9cbfedceec41b30cf54b859226bcab6a1f
xuancanhit99 <xuancanhit99@gmail.com>
2021-10-29 03:06:05 +0300

    Initial commit

---
+++ src/main/java/com/example/game15puzzle/Game15PuzzleApplication.java
@@ -0,0 +1,13 @@
+package com.example.game15puzzle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Game15PuzzleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Game15PuzzleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/game15puzzle/Game15PuzzleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.game15puzzle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Game15PuzzleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
