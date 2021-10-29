a661f33b5d73c3b194c7fa833147ce378ac84cd0
xuancanhit99 <xuancanhit99@gmail.com>
2021-10-29 20:06:39 +0300

    Write Test Case 1 for Init() function

--- src/test/java/com/example/game15puzzle/Game15PuzzleApplicationTests.java
+++ src/test/java/com/example/game15puzzle/Game15PuzzleApplicationTests.java
@@ -3,11 +3,35 @@ package com.example.game15puzzle;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
 class Game15PuzzleApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void GamePlayInitTestCase1() {
+        GamePlay gamePlay = new GamePlay();
+        int[][] arrTest = new int[4][4];
+        arrTest[0][0] = 1;
+        arrTest[0][1] = 2;
+        arrTest[0][2] = 13;
+        arrTest[0][3] = 14;
+        arrTest[1][0] = 15;
+        arrTest[1][1] = 6;
+        arrTest[1][2] = 7;
+        arrTest[1][3] = 8;
+        arrTest[2][0] = 9;
+        arrTest[2][1] = 10;
+        arrTest[2][2] = 11;
+        arrTest[2][3] = 12;
+        arrTest[3][0] = 3;
+        arrTest[3][1] = 4;
+        arrTest[3][2] = 5;
+        gamePlay.setNumbers(arrTest);
+        gamePlay.Init();
+        // Desired result of array
+        int[][] arrResult = new int[4][4]; //(All values in array = 0)
+        assertThat(gamePlay.getNumbers()).isEqualTo(arrResult);
     }
 
 }
