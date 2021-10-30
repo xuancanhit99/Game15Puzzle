031453231e17b05a9fd9ca68ab9346c456a2c707
xuancanhit99 <xuancanhit99@gmail.com>
2021-10-30 03:04:13 +0300

    Declare the variables of the application's interface

--- src/main/java/com/example/game15puzzle/GamePlay.java
+++ src/main/java/com/example/game15puzzle/GamePlay.java
@@ -1,9 +1,28 @@
 package com.example.game15puzzle;
 
 
+import javax.swing.*;
 import java.util.Random;
 
 public class GamePlay {
+
+    JButton jButtonPlayRTP, jButtonStartRTP, jButtonPlay15P, jButton15PMixNum, jButton1, jButton2, jButton3, jButton4, jButton5, jButton6, jButton7, jButton8, jButton9;
+    JLabel jLabel1, jLabel2, jLabel3, jLabel4, jLabel5, jLabel6;
+    JScrollPane jScrollPaneRTP, jScrollPane15P, jScrollPaneTable;
+    JTextArea jTextAreaRTP, jTextArea15P;
+    JTable jTableRTP;
+    JTabbedPane jTabbedPaneGameMain;
+    JPanel jPanelRTP, jPanel15P, jPanel15pBottom;
+
+    JFrame jFrameRuleRTP, jFrameRule15P, jFrameGameMain;
+
+    GroupLayout jFrameRTPLayout, jFrame15PLayout;
+
+    JMenu jMenuMainGame, jMenuGame;
+    JMenuBar jMenuBarMain;
+    JMenuItem jMenuItemGameRTP, jMenuItemGame15P, jMenuItemExit;
+
+
     int[] invariants = new int[16];
     private static Random generator = new Random();
     private int[][] numbers = new int[4][4];
@@ -22,6 +41,15 @@ public class GamePlay {
 
     public void setInvariants(int[] invariants) {
         this.invariants = invariants;
+    }
+
+
+    public GamePlay() {
+        initComponents();
+    }
+
+    private void initComponents() {
+
     }
 
     //Re-initialize array
