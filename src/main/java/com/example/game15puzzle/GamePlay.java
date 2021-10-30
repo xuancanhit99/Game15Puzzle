e3bbd992fe140672fe9a310c0c89a226ab3ea870
xuancanhit99 <xuancanhit99@gmail.com>
2021-10-30 03:29:29 +0300

    Optimizing the application

--- src/main/java/com/example/game15puzzle/GamePlay.java
+++ src/main/java/com/example/game15puzzle/GamePlay.java
@@ -1,6 +1,4 @@
 package com.example.game15puzzle;
-
-
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
@@ -8,8 +6,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Random;
 
-public class GamePlay {
-
+public class GamePlay extends JFrame {
     JButton jButtonPlayRTP, jButtonStartRTP, jButtonPlay15P, jButton15PMixNum, jButton1, jButton2, jButton3, jButton4, jButton5, jButton6, jButton7, jButton8, jButton9;
     JLabel jLabel1, jLabel2, jLabel3, jLabel4, jLabel5, jLabel6;
     JScrollPane jScrollPaneRTP, jScrollPane15P, jScrollPaneTable;
@@ -26,27 +23,8 @@ public class GamePlay {
     JMenuBar jMenuBarMain;
     JMenuItem jMenuItemGameRTP, jMenuItemGame15P, jMenuItemExit;
 
-
-    int[] invariants = new int[16];
     private static Random generator = new Random();
     private int[][] numbers = new int[4][4];
-
-    public int[][] getNumbers() {
-        return numbers;
-    }
-
-    public void setNumbers(int[][] numbers) {
-        this.numbers = numbers;
-    }
-
-    public int[] getInvariants() {
-        return invariants;
-    }
-
-    public void setInvariants(int[] invariants) {
-        this.invariants = invariants;
-    }
-
 
     public GamePlay() {
         initComponents();
@@ -55,7 +33,9 @@ public class GamePlay {
     // Game Rearrange The Pieces: RTP - Игра "переставь фигуры"
     // Game 15 puzzle: 15P - Игра "Пятнашки"
 
+
     private void initComponents() {
+
         jButton1 = new JButton();
         jButton2 = new JButton();
         jButton3 = new JButton();
@@ -83,6 +63,7 @@ public class GamePlay {
         jTextAreaRTP = new JTextArea();
         jFrameRule15P = new JFrame();
         jTextArea15P = new JTextArea();
+
 
         jButton15PMixNum = new JButton();
         //---Game RTP---
@@ -201,11 +182,11 @@ public class GamePlay {
         jScrollPaneTable = new JScrollPane();
         jScrollPaneTable.setViewportView(jTableRTP);
 
+
         //Tab in Main Game
         jTabbedPaneGameMain = new JTabbedPane();
         jTabbedPaneGameMain.addTab("Переставь фигуры", jPanelRTP);
         jTabbedPaneGameMain.addTab("Пятнашки", jPanel15P);
-
 
         //Tab 15P
         GridLayout jPanel15PLayout = new GridLayout(5, 4, 2, 2);
@@ -221,6 +202,7 @@ public class GamePlay {
                 repaintField();
             }
         });
+
 
         //Tab RTP
         GroupLayout jPanelRTPLayout = new GroupLayout(jPanelRTP);
@@ -455,7 +437,6 @@ public class GamePlay {
 
         setJMenuBar(jMenuBarMain);
 
-
         //Size
         GroupLayout layout = new GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -471,7 +452,6 @@ public class GamePlay {
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setTitle("Mini Chess & 15 Puzzle");
         setLocationRelativeTo(null);
-
     }
 
     private void init() {
@@ -540,6 +520,20 @@ public class GamePlay {
         jButton15PMixNum.setText("Перемешать");
         jPanel15pBottom.add(jButton15PMixNum);
         jPanel15P.validate();
+    }
+
+    public boolean checkWin() {
+        boolean status = true;
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                if (i == 3 && j > 2)
+                    break;
+                if (numbers[i][j] != i * 4 + j + 1) {
+                    status = false;
+                }
+            }
+        }
+        return status;
     }
 
     private class ClickListener implements ActionListener {
@@ -692,22 +686,6 @@ public class GamePlay {
         jFrameRuleRTP.setVisible(true);
     }
 
-
-
-    public boolean checkWin() {
-        boolean status = true;
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 4; j++) {
-                if (i == 3 && j > 2)
-                    break;
-                if (numbers[i][j] != i * 4 + j + 1) {
-                    status = false;
-                }
-            }
-        }
-        return status;
-    }
-
     private void jButtonPlayRTPActionPerformed(ActionEvent e) {
         jFrameGameMain.setVisible(true);
         jFrameRuleRTP.dispose();
@@ -799,106 +777,5 @@ public class GamePlay {
             jButton9.setEnabled(false);
         }
     }
-
-    //Re-initialize array
-    public void Init() {
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 4; j++) {
-                numbers[i][j] = 0;
-                invariants[i * 4 + j] = 0;
-            }
-        }
-    }
-
-    //The values of the elements in the array are random
-    public void RandArr() {
-        for (int i = 1; i < 16; i++) {
-            int k;
-            int l;
-            do {
-                k = generator.nextInt(100) % 4;
-                l = generator.nextInt(100) % 4;
-            }
-            while (numbers[k][l] != 0);
-            numbers[k][l] = i;
-            invariants[k * 4 + l] = i;
-        }
-    }
-
-    public void CheckArrValid() {
-        boolean change = true;
-        int counter = 1;
-        while (change) {
-            change = false;
-            for (int i = 0; i < 16; i++) {
-                if (invariants[i] != i) {
-                    for (int j = 0; j < 16; j++) {
-                        if (invariants[j] == i) {
-                            int temp = invariants[i];
-                            invariants[i] = invariants[j];
-                            invariants[j] = temp;
-                            change = true;
-                            counter++;
-                            break;
-                        }
-                    }
-                    break;
-                }
-            }
-        }
-        System.out.println();
-        System.out.println(counter);
-        if (counter % 2 != 0) {
-            int temp = numbers[0][0];
-            numbers[0][0] = numbers[3][3];
-            numbers[3][3] = temp;
-        }
-    }
-
-
-    public boolean CheckWin() {
-        boolean status = true;
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 4; j++) {
-                if (i == 3 && j > 2)
-                    break;
-                if (numbers[i][j] != i * 4 + j + 1) {
-                    status = false;
-                }
-            }
-        }
-        return status;
-    }
-
-    public void PrintArr() {
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 4; j++) {
-                System.out.print(numbers[i][j] + " ");
-            }
-            System.out.println();
-        }
-        System.out.println();
-    }
-
-    public void Print() {
-        for (int i = 0; i < 16; i++) {
-            System.out.print(invariants[i] + " ");
-        }
-        System.out.println();
-    }
-
-    public static void main(String[] args) {
-        GamePlay gamePlay = new GamePlay();
-        gamePlay.Init();
-        gamePlay.Print();
-
-        gamePlay.RandArr();
-        gamePlay.Print();
-        gamePlay.PrintArr();
-
-        gamePlay.CheckArrValid();
-        gamePlay.Print();
-        gamePlay.PrintArr();
-    }
-
 }
+
